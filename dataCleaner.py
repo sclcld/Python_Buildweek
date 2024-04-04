487b9470ed1fc68d91d3606de2efec9017347f3d
Claudio Scalia <129424657+sclcld@users.noreply.github.com>
2024-04-04 02:16:39 +0200

    Files Updated
    DataExtractor Updated, added DataCleaner module, raw txt files and obtained csvs.

---
+++ dataCleaner.py
@@ -0,0 +1,114 @@
+modelli_melluso = ""
+prezzi_melluso = ""
+modelli_lumb= ""
+
+#il file lumberjack prices è già pulito e in formato stringa
+prezzi_lumb_string = ""
+
+with open("melluso_models_raw.txt", "r") as file:
+
+    modelli_melluso = file.read().splitlines()
+
+with open("melluso_prices_raw.txt", "r") as file:
+
+    prezzi_melluso = file.read().splitlines()
+
+with open("lumberjack_models_raw.txt", "r") as file:
+
+    modelli_lumb = file.read().splitlines()
+
+with open("lumberjack_prices_raw.txt", "r") as file:
+
+    prezzi_lumb_string = file.read().splitlines()
+
+
+# Lumberjack
+# Pattern : THELMA SNEAKERS DONNA
+def lumb_cleaner() -> list:
+
+    clean_models = []
+
+    for mod in modelli_lumb:
+        #paired = THELMA SNEAKERS
+        paired = mod.split()[:-1]
+        #*s_type= MODELLO len >= 1
+        name, *s_type = paired
+        #["Thelma", "Sneakers"]
+        clean_models.append([name.capitalize(), " ".join(s_type).capitalize()])
+    
+    return clean_models
+
+
+# Melluso
+#           0         1     2         3  4        5        6(-1)  7   
+# Pattern : Décolleté donna slingback in pelle    nappetta nero   d166w
+#           Type                         Material         Color
+#  Type = Pattern[0], 
+#  Material = Pattern[index "in" + 1], 
+#  Color = Pattern[-1]
+
+# in alcune stringhe "in" viene riportato come "il" 
+
+def melluso_cleaner():
+
+    rows = []
+
+    for string in modelli_melluso:
+        
+        index = 0
+        splitted = string.split()[:-1] 
+        
+        for i, stop in enumerate(splitted):
+
+            if stop == "il" or stop == "in":
+
+                index = i
+        
+        model = splitted[0].strip()
+        material = splitted[index + 1]
+        color = splitted[-1]
+        rows.append([model,material, color])
+    
+    return rows
+ 
+modelli_melluso_clean = melluso_cleaner()
+modelli_lumb_clean = lumb_cleaner()
+# pattern '89,90\xa0€'
+prezzi_melluso_numeric = [float(prezzo[:-2].replace(",", ".")) for prezzo in prezzi_melluso]
+# pattern '49.99'
+prezzi_lumb_numeric = [float(price) for price in prezzi_lumb_string]
+
+
+def melluso_csv_maker():
+    
+    with open("melluso.csv", "w") as file:
+            
+        for index, data in enumerate(zip(modelli_melluso_clean, prezzi_melluso_numeric)):
+        
+            model, mat, color = data[0]
+            price = data[1]
+
+            if index == 0:
+            
+                file.write("Modello,Materiale,Colore,Prezzo\n")
+
+            file.write(f"{model},{mat},{color},{price}\n")     
+
+
+def lumberjack_csv_maker():
+    
+    with open("lumberjack.csv", "w") as file:
+        
+        for index, data in enumerate(zip(modelli_lumb_clean, prezzi_lumb_string)):
+
+            model_name, model = data[0]
+            price = data[-1]
+            
+            if index == 0 : 
+                
+                file.write("Nome, Modello, Prezzo\n") 
+            
+            file.write(f"{model_name},{model},{price}\n")
+
+melluso_csv_maker()
+lumberjack_csv_maker()
---
+++ dataExtractor.py
@@ -0,0 +1,93 @@
+import requests
+from bs4 import BeautifulSoup
+
+# Data Extractor è creato per rendere automatiche le operazioni di estrazione dati 
+# da cataloghi web. Le sue funzioni sono molto limitate ma renderà meno macchinose
+# le operazioni con Beautiful Soup. Basterà inserire l'url del catalogo, un suffisso
+# ed un range ed estrarrà le pagine desiderate. Utilizzando il find_all() di BeautifulSoup
+# sarà in grado di estrarre i dati desiderati restituendoli in una lista.
+
+class DataExtractor:
+
+    def __init__(self, url: str, subfix: str, pages: int) -> None:
+
+        self.url = url
+        self.subf= subfix
+        self.max_range= pages
+        self.pages= []
+        self.pages_extractor()
+        
+    def pages_extractor(self) -> bool:
+
+        for x in range(1, self.max_range + 1):
+            
+            incr_url = f"{self.url}{'' if x == 1 else self.subf + str(x)}"
+            req = requests.get(incr_url)
+            
+            if "200" not in str(req):
+                print("One or More Requests Not Accepted. Interrupting operation")
+                self.pages = []
+                
+                return False
+            
+            else:
+                self.pages.append(req)
+        
+        print("All requests accepted")
+        return True
+
+    def data_ext(self, type: str, classes: list):
+        
+        strings = []
+        for req in self.pages:
+            
+            bf_obj = BeautifulSoup(req.text, "html.parser").find_all(type, class_= classes)
+            
+            for x in bf_obj:
+                
+                strings.append(x.text.strip())
+        
+        if strings:
+            return strings
+
+        print("Operation Failed")
+
+        return False  
+    
+
+def main():
+
+    melluso_url = "https://www.melluso.com/it/melluso-donna/nuova-collezione.html"
+    melluso_subfix = '?p='
+    lumberjack_url = "https://www.lumberjack.com/it/campaign/woman?category=shoes"
+    lumberjack_subfix = "&page="
+
+    melluso_data= DataExtractor(melluso_url, melluso_subfix, 25)
+    melluso_models = melluso_data.data_ext("a",["product-item-link"])
+    melluso_prices = melluso_data.data_ext("span", ["price"])
+
+    lumberjack_data = DataExtractor(lumberjack_url, lumberjack_subfix, 7)
+    lumberjack_models = lumberjack_data.data_ext("div",["product__name"])
+    lumberjack_prices = [float(x[:-4].replace(",",".")) for x in lumberjack_data.data_ext("span", ["product__prices-sale"])]
+
+    # L'operazione di scraping può essere molto lenta. Con to_txt, inserendo come stringa il nome che vorremo
+    # dare al file, verranno prodotti dei file txt con tutti i dati estratti
+
+    def to_txt(filename: str, file: list):
+
+         with open(f"{filename}_raw.txt", "w") as file1:
+             for item in file:
+
+                file1.write(str(item) + "\n")
+
+
+    to_txt("melluso_models", melluso_models)
+    to_txt("melluso_prices", melluso_prices)
+    to_txt("lumberjack_models", lumberjack_models)
+    to_txt("lumberjack_prices", lumberjack_prices)
+
+
+if __name__ == "__main__":
+    
+    main()
+
